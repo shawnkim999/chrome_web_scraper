be3d773001fcb9ecfb3ca65054b8023a2ece3630
Shawn <shawnkimprojects@gmail.com>
2024-10-05 01:42:17 -0700

    more files

---
+++ testing.js
@@ -0,0 +1,19 @@
+const { chromium } = require('playwright');
+
+(async () => {
+  const pathToExtension = require('path').join(__dirname, 'my-extension');
+  const userDataDir = '/tmp/test-user-data-dir';
+  const browserContext = await chromium.launchPersistentContext(userDataDir, {
+    headless: false,
+    args: [
+      `--disable-extensions-except=${pathToExtension}`,
+      `--load-extension=${pathToExtension}`
+    ]
+  });
+  let [backgroundPage] = browserContext.backgroundPages();
+  if (!backgroundPage)
+    backgroundPage = await browserContext.waitForEvent('backgroundpage');
+
+  // Test the background page as you would any other page.
+  await browserContext.close();
+})();
